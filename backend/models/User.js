bdcbda5f5bbefe5543f8bb3ef5b23f20f4164868
Koushik Debnath <koushikcomhere@gmail.com>
2026-02-01 02:13:00 +0530

    user model created

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,10 +1,24 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    isAdmin: { type: Boolean, required: true, default: false },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    }
 }, {
     timestamps: true
 });

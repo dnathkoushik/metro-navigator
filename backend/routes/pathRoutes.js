afab5bd8ff0bfb69a002c03ddb27d59031132205
Koushik Debnath <koushikcomhere@gmail.com>
2026-02-02 11:29:48 +0530

    Route for minimum time created

--- backend/routes/pathRoutes.js
+++ backend/routes/pathRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getMinStationsPath } = require('../controllers/pathController');
-
+const { getMinStationsPath, getMinTimePath } = require('../controllers/pathController');
 // @route   POST /api/path/min-stations
 // @desc    Get shortest path with minimum number of stations
 router.post('/min-stations', getMinStationsPath);
+
+// @route   POST /api/path/min-time
+// @desc    Get shortest path with minimum time
+router.post('/min-time', getMinTimePath);
 
 module.exports = router;

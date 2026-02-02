a7d3e092c1fb0ffab49ac2764666e58019f3ecd5
Koushik Debnath <koushikcomhere@gmail.com>
2026-02-02 11:48:48 +0530

    some changes

--- backend/routes/pathRoutes.js
+++ backend/routes/pathRoutes.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const { getMinStationsPath, getMinTimePath, getMinDistancePath } = require('../controllers/pathController');
+const { protect } = require('../middleware/authMiddleware');
 // @route   POST /api/path/min-stations
 // @desc    Get shortest path with minimum number of stations
-router.post('/min-stations', getMinStationsPath);
+router.post('/min-stations', protect, getMinStationsPath);
 
 // @route   POST /api/path/min-time
 // @desc    Get shortest path with minimum time
-router.post('/min-time', getMinTimePath);
+router.post('/min-time', protect, getMinTimePath);
 
 // @route   POST /api/path/min-distance
 // @desc    Get shortest path with minimum distance
-router.post('/min-distance', getMinDistancePath);
+router.post('/min-distance', protect, getMinDistancePath);
 
 module.exports = router;

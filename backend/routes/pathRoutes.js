771ab49bc3a5510206b06ad12a95abc7138dc249
Koushik Debnath <koushikcomhere@gmail.com>
2026-02-02 11:38:01 +0530

    route for shortest distance path has been created

--- backend/routes/pathRoutes.js
+++ backend/routes/pathRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getMinStationsPath, getMinTimePath } = require('../controllers/pathController');
+const { getMinStationsPath, getMinTimePath, getMinDistancePath } = require('../controllers/pathController');
 // @route   POST /api/path/min-stations
 // @desc    Get shortest path with minimum number of stations
 router.post('/min-stations', getMinStationsPath);
@@ -8,5 +8,9 @@ router.post('/min-stations', getMinStationsPath);
 // @route   POST /api/path/min-time
 // @desc    Get shortest path with minimum time
 router.post('/min-time', getMinTimePath);
+
+// @route   POST /api/path/min-distance
+// @desc    Get shortest path with minimum distance
+router.post('/min-distance', getMinDistancePath);
 
 module.exports = router;
